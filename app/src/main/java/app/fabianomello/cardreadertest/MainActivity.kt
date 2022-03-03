c4b32d57b56d9b3adccc8787b4e0792c16bbd947
Fabiano Mello <f.augmello@hotmail.com>
2022-03-03 14:57:26 -0300

    Initial commit

---
+++ app/src/main/java/app/fabianomello/cardreadertest/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.fabianomello.cardreadertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
